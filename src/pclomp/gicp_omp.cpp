129c2d3d687676e9444a81cb90087a98c20f9b81
SakodaShintaro <SakodaShintaro@users.noreply.github.com>
2024-02-15 05:17:34 +0000

    style(pre-commit): autofix

--- src/pclomp/gicp_omp.cpp
+++ src/pclomp/gicp_omp.cpp
@@ -2,4 +2,4 @@
 #include <pclomp/gicp_omp_impl.hpp>
 
 template class pclomp::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>;
-template class pclomp::GeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI>;    
+template class pclomp::GeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI>;
