822e861ea5697ee7d8704fd1cca7815357d5bfa9
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-05-02 13:30:03 +0900

    Added norm search
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- src/bbs3d/bbs3d_search.cpp
+++ src/bbs3d/bbs3d_search.cpp
@@ -23,28 +23,19 @@ SearchResult bbs3d_search(std::shared_ptr<NormalDistributionsTransform> ndt_ptr,
   const Eigen::Matrix4f initial_pose = pose_to_matrix4f(initial_pose_with_cov.pose.pose);
   const pcl::PointCloud<pcl::PointXYZ>::ConstPtr source_cloud = ndt_ptr->getInputSource();
 
-  BBS3D bbs3d;
-
-  std::vector<Eigen::Vector3d> src_points;
-  double max_norm = 0.0;
-  const double kLimitNorm = 20.0;
+  float max_norm = 0.0;
   for(const auto& point : source_cloud->points) {
-    const double norm = std::hypot(point.x, point.y, point.z);
-    if(norm > kLimitNorm) {
-      continue;
-    }
-    src_points.emplace_back(point.x, point.y, point.z);
-    max_norm = std::max(max_norm, src_points.back().norm());
+    max_norm = std::max(max_norm, std::hypot(point.x, point.y, point.z));
   }
-  bbs3d.set_src_points(src_points);
 
   const double kSearchWidth = 5.0;
   const double cloud_width = kSearchWidth + max_norm;
 
+  BBS3D bbs3d;
+
+  // set target points
   const pcl::PointCloud<pcl::PointXYZ>::ConstPtr target_cloud_const = ndt_ptr->getInputTarget();
   pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud(new pcl::PointCloud<pcl::PointXYZ>(*target_cloud_const));
-
-  // filter target_cloud
   pcl::PassThrough<pcl::PointXYZ> pass_x;
   pass_x.setInputCloud(target_cloud);
   pass_x.setFilterFieldName("x");
@@ -55,19 +46,13 @@ SearchResult bbs3d_search(std::shared_ptr<NormalDistributionsTransform> ndt_ptr,
   pass_y.setFilterFieldName("y");
   pass_y.setFilterLimits(initial_pose(1, 3) - cloud_width, initial_pose(1, 3) + cloud_width);
   pass_y.filter(*target_cloud);
-
   std::vector<Eigen::Vector3d> target_points;
   for(const auto& point : target_cloud->points) {
     target_points.emplace_back(point.x, point.y, point.z);
   }
-
   const double min_level_res = 0.5;
   const int max_level = 4;
   bbs3d.set_tar_points(target_points, min_level_res, max_level);
-
-  // other settings
-  bbs3d.set_score_threshold_percentage(0.25);
-  bbs3d.enable_timeout();
 
   // set search range
   Eigen::Vector3d min_xyz = Eigen::Vector3d::Constant(std::numeric_limits<double>::max());
@@ -82,21 +67,42 @@ SearchResult bbs3d_search(std::shared_ptr<NormalDistributionsTransform> ndt_ptr,
   max_xyz.y() = initial_pose(1, 3) + kSearchWidth;
   bbs3d.set_trans_search_range(min_xyz, max_xyz);
 
+  // other settings
+  bbs3d.set_score_threshold_percentage(0.25);
+  bbs3d.enable_timeout();
   bbs3d.set_timeout_duration_in_msec(1000);
 
-  bbs3d.localize_by_chokudai_search();
-
-  const int best_score = bbs3d.get_best_score();
-
-  const Eigen::Matrix4d global_pose = bbs3d.get_global_pose();
-
-  ndt_ptr->align(*target_cloud, global_pose.cast<float>());
-  const pclomp::NdtResult ndt_result = ndt_ptr->getResult();
-  const Eigen::Matrix4f ndt_result_pose = ndt_result.pose;
-
   SearchResult result;
-  result.pose_with_cov.pose.pose = matrix4f_to_pose(ndt_result_pose);
-  result.score = ndt_result.transform_probability;
+  result.score = 0.0;
+
+  for(int div_pow_2 = 0; div_pow_2 < 3; div_pow_2++) {
+    // set curr src points
+    const double limit_norm = max_norm / (1 << div_pow_2);
+    std::vector<Eigen::Vector3d> src_points;
+    for(const auto& point : source_cloud->points) {
+      const double norm = std::hypot(point.x, point.y, point.z);
+      if(norm > limit_norm) {
+        continue;
+      }
+      src_points.emplace_back(point.x, point.y, point.z);
+    }
+    bbs3d.set_src_points(src_points);
+
+    // search
+    bbs3d.localize_by_chokudai_search();
+    const int best_score = bbs3d.get_best_score();
+    const Eigen::Matrix4d global_pose = bbs3d.get_global_pose();
+
+    // align
+    ndt_ptr->align(*target_cloud, global_pose.cast<float>());
+    const pclomp::NdtResult ndt_result = ndt_ptr->getResult();
+    const Eigen::Matrix4f ndt_result_pose = ndt_result.pose;
+    if(ndt_result.transform_probability > result.score) {
+      result.pose_with_cov.pose.pose = matrix4f_to_pose(ndt_result_pose);
+      result.score = ndt_result.transform_probability;
+    }
+  }
+
   return result;
 }
 
